165a8f86f67eb1062e79f8d31a4a19ee03d7d1fd
blueblank <blueblank@gmail.com>
2013-04-04 11:16:58 -0400

    version bump

--- flask_macro4/__init__.py
+++ flask_macro4/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from macro4 import (Macro4, MacroFor, AccordianItem, AccordianGroupMacro,
     BreadCrumbItem, BreadCrumbMacro, TabItem, TabGroupMacro)

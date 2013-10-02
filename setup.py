2bf6d8d4034917797f4aa3e8fa04a558dfd22962
blueblank <blueblank@gmail.com>
2013-10-02 15:42:20 -0400

    Bump version number to 0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Macro4',
-    version='0',
+    version='0.0.6',
     url='https://github.com/thrisp/flask_macro4',
     license='MIT',
     author='hurrata/thrisp',

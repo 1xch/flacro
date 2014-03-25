cc3a4c132716baac92267d70c3a7b41c6cddc342
blueblank <blueblank@gmail.com>
2014-03-25 12:22:01 -0400

    Bump version number to 0.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ copyright = project_copyright
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.7'
+version = '0.0.8'
 # The full version, including alpha/beta/rc tags.
 release = version
 
